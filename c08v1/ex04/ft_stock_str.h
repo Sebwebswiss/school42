d37859b36f719a33cde0ee26b2693dcde4c181af
sebwebswss <84191341+Sebwebswiss@users.noreply.github.com>
2023-08-03 16:37:06 +0200

    Add files via upload

---
+++ c08v1/ex04/ft_stock_str.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stock_str.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sezunec <marvin@42lausanne.ch>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/03 15:27:57 by sezunec           #+#    #+#             */
+/*   Updated: 2023/08/03 15:44:48 by sezunec          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_STOCK_STR_H
+# define FT_STOCK_STR_H
+
+typedef struct s_stock_str
+{
+	int		size;
+	char	*str;
+	char	*copy;
+}				t_stock_str;
+
+#endif
